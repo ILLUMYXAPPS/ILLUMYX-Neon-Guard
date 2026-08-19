708933f09a540940ec98e665bfeb0d7fd0a5666d
Aaron Paszek <aaronpaszek@icloud.com>
2026-08-19 23:10:48 +1000

    security: export trusted-device token controls

--- security/index.ts
+++ security/index.ts
@@ -1,6 +1,8 @@
 export { authorizeAuthentication } from './AuthenticationGuard';
 export { authorizeOwnershipChange, CANONICAL_OWNER } from './OwnershipControl';
+export { issueTrustedDeviceToken, tokenDigest, validateTrustedDeviceToken } from './TrustedDeviceToken';
 export { InMemorySecurityLogger } from './SecurityLogger';
 export type { OwnershipAuthorization, OwnershipChangeDecision, OwnershipState } from './OwnershipControl';
+export type { TrustedDeviceTokenRecord, TrustedDeviceTokenStore, TokenValidationResult } from './TrustedDeviceToken';
 export type { SecurityLogger } from './SecurityLogger';
 export type { SecurityEvent, SecurityEventType } from './SecurityEvent';
