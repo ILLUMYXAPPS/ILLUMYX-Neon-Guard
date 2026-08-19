6869158d59d29e1021bf113b35ae0a86ad8909ca
Aaron Paszek <aaronpaszek@icloud.com>
2026-08-19 23:12:01 +1000

    security: expose server authorization boundary

--- security/index.ts
+++ security/index.ts
@@ -1,8 +1,10 @@
 export { authorizeAuthentication } from './AuthenticationGuard';
 export { authorizeOwnershipChange, CANONICAL_OWNER } from './OwnershipControl';
 export { issueTrustedDeviceToken, tokenDigest, validateTrustedDeviceToken } from './TrustedDeviceToken';
+export { authorizeServerRequest } from './ServerAuthorization';
 export { InMemorySecurityLogger } from './SecurityLogger';
 export type { OwnershipAuthorization, OwnershipChangeDecision, OwnershipState } from './OwnershipControl';
 export type { TrustedDeviceTokenRecord, TrustedDeviceTokenStore, TokenValidationResult } from './TrustedDeviceToken';
+export type { ServerAuthorizationRequest, ServerAuthorizationDecision, AuthorizationAuditSink } from './ServerAuthorization';
 export type { SecurityLogger } from './SecurityLogger';
 export type { SecurityEvent, SecurityEventType } from './SecurityEvent';
