0a7857c24fb0c1952db328ff89a63dcba0f6065a
Aaron Paszek <aaronpaszek@icloud.com>
2026-08-19 19:30:14 +1000

    Fix Swift Package platform availability for CI

--- ios/Package.swift
+++ ios/Package.swift
@@ -4,7 +4,8 @@ import PackageDescription
 let package = Package(
     name: "NeonGuardClient",
     platforms: [
-        .iOS(.v16)
+        .iOS(.v16),
+        .macOS(.v12)
     ],
     products: [
         .library(name: "NeonGuardClient", targets: ["NeonGuardClient"])
