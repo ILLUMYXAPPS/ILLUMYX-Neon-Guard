48f593e37dda42f66c62cfc2697be06f2531fe0f
Aaron Paszek <aaronpaszek@icloud.com>
2026-08-19 23:06:30 +1000

    security: export ownership control

--- security/index.ts
+++ security/index.ts
@@ -1,4 +1,6 @@
 export { authorizeAuthentication } from './AuthenticationGuard';
+export { authorizeOwnershipChange, CANONICAL_OWNER } from './OwnershipControl';
 export { InMemorySecurityLogger } from './SecurityLogger';
+export type { OwnershipAuthorization, OwnershipChangeDecision, OwnershipState } from './OwnershipControl';
 export type { SecurityLogger } from './SecurityLogger';
 export type { SecurityEvent, SecurityEventType } from './SecurityEvent';
